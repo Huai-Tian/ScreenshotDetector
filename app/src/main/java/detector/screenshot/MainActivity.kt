c9ff4d82d52cde94443dbab518a80df1f5c6d89f
怀天 <2965373537@qq.com>
2026-07-11 14:42:42 +0800

    Initial commit

---
+++ app/src/main/java/detector/screenshot/MainActivity.kt
@@ -0,0 +1,18 @@
+package detector.screenshot
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.Text
+import androidx.compose.ui.res.stringResource
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Text(stringResource(R.string.app_name))
+        }
+    }
+}
